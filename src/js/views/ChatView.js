e0ab43c07446def843f8a46830b431814d5ae8d1
Tasnimul Hasan <589.dreamer.589@gmail.com>
2022-07-17 01:43:31 +0600

    baaaki kaal dekhlenge

--- src/js/views/ChatView.js
+++ src/js/views/ChatView.js
@@ -1,76 +1,79 @@
-import markup from "../../components/chat.html"
-import { HTML } from "../HELPER"
-import { Views } from "./Views"
+import markup from '../../components/chat.html';
+import { HTML } from '../HELPER';
+import { Views } from './Views';
 
 class Chat extends Views {
   constructor() {
-    super()
+    super();
   }
 
-  _element = new HTML(markup)
+  _element = new HTML(markup);
 
-  #messageContainer = this._element.querySelector(`#chat-container`)
+  #messageContainer = this._element.querySelector(`#chat-container`);
 
   #generateMessageMarkup(data) {
     const element = new HTML(`
-      <div>
-        ${data.name}: ${data.msg} 
+      <div class="message">
+        <span>${data.name}</span>
+        <p>${data.msg}<p>         
       </div>
-      `)
+      `);
 
     if (data._id) {
-      element.dataset.id = data._id
-      const isMsgAlreadyRendered = this.#messageContainer.querySelector(`[data-id="${data._id}"]`)
-      if (isMsgAlreadyRendered) return false
+      element.dataset.id = data._id;
+      const isMsgAlreadyRendered = this.#messageContainer.querySelector(
+        `[data-id="${data._id}"]`
+      );
+      if (isMsgAlreadyRendered) return false;
     } else {
-      element.dataset.status = "pending"
+      element.dataset.status = 'pending';
     }
 
     if (data.you) {
-      element.dataset.user = "you"
+      element.dataset.user = 'you';
     }
 
-    element.title = `<${data.name}>${data.email}`
-    return element
+    element.title = `<${data.name}>${data.email}`;
+    return element;
   }
 
   addLoadMoreHandler(callback) {
     // TODO
     // Add an event to fix me...
 
-    const oldestMessage = this.#messageContainer.firstElementChild
-    callback(oldestMessage.dataset.id)
+    const oldestMessage = this.#messageContainer.firstElementChild;
+    callback(oldestMessage.dataset.id);
   }
 
   appendMessage(data) {
-    const element = this.#generateMessageMarkup(data)
+    const element = this.#generateMessageMarkup(data);
 
-    if (!element) return
-    this.#messageContainer.appendChild(element)
-    return element
+    if (!element) return;
+    this.#messageContainer.appendChild(element);
+    return element;
   }
 
   prependMessage(data) {
-    const element = this.#generateMessageMarkup(data)
-    if (!element) return
-    this.#messageContainer.prepend(element)
+    const element = this.#generateMessageMarkup(data);
+    if (!element) return;
+    this.#messageContainer.prepend(element);
   }
 
   appendMessageSent(element, id) {
-    element.dataset.id = id
-    element.dataset.status = "sent"
+    element.dataset.id = id;
+    element.dataset.status = 'sent';
   }
 
   addMsgSubmitHandler(callback) {
     this._element.querySelector(`#chat-form`).onsubmit = () => {
-      event.preventDefault()
-      const { msg } = event.target
-      callback(msg.value)
-      msg.value = ""
-    }
+      event.preventDefault();
+      const { msg } = event.target;
+      callback(msg.value);
+      msg.value = '';
+    };
   }
 
   addLogoutHandler(callback) {}
 }
 
-export default new Chat()
+export default new Chat();
