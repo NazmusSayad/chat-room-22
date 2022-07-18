9b3a4d44539feac69fb32840c1a7fda3f549d8f2
Tasnimul Hasan <589.dreamer.589@gmail.com>
2022-07-19 03:57:32 +0600

    theme toggler added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,15 @@
-import { blockBadWords, deleteDuplicateNewLines } from "./HELPER.js"
-import * as model from "./model.js"
-import ChatView from "./views/ChatView.js"
-import LoginView from "./views/LoginView.js"
-import SignupView from "./views/SignupVIew.js"
-import WelcomeView from "./views/WelcomeView.js"
+import { blockBadWords, deleteDuplicateNewLines } from './HELPER.js'
+import * as model from './model.js'
+import ChatView from './views/ChatView.js'
+import LoginView from './views/LoginView.js'
+import SignupView from './views/SignupVIew.js'
+import WelcomeView from './views/WelcomeView.js'
 
 const loginPage = () => LoginView.render()
 
 const signupPage = () => SignupView.render()
 
-const loginSubmit = async (token) => {
+const loginSubmit = async token => {
   try {
     await model.login(token)
     initChat()
@@ -19,7 +19,7 @@ const loginSubmit = async (token) => {
   }
 }
 
-const signupSubmit = async (token) => {
+const signupSubmit = async token => {
   try {
     await model.signUp(token)
     initChat()
@@ -29,7 +29,7 @@ const signupSubmit = async (token) => {
   }
 }
 
-const sendMessage = async (msg) => {
+const sendMessage = async msg => {
   try {
     msg = deleteDuplicateNewLines(msg)
     msg = blockBadWords(msg)
@@ -45,15 +45,15 @@ const sendMessage = async (msg) => {
     ChatView.appendMessageSent(element, data)
   } catch (err) {
     console.error(err)
-    alert("Unable to send message.\nPlease reload this page.")
+    alert('Unable to send message.\nPlease reload this page.')
   }
 }
 
-const loadMoreMessages = async (oldestMessage) => {
+const loadMoreMessages = async oldestMessage => {
   try {
     const id = oldestMessage.dataset.id
     const data = await model.getMessageById(id)
-    data.forEach((msg) => {
+    data.forEach(msg => {
       msg.you = msg.email === model.STATE.user.email
       ChatView.prependMessage(msg)
     })
@@ -77,7 +77,7 @@ const initChat = async () => {
     )
 
     const data = await model.getMessage()
-    data.reverse().forEach((msg) => {
+    data.reverse().forEach(msg => {
       msg.you = msg.email === model.STATE.user.email
       ChatView.appendMessage(msg)
     })
@@ -104,12 +104,24 @@ const initChat = async () => {
   ChatView.addMsgSubmitHandler(sendMessage)
   ChatView.addTextAreaHandlers()
 
-  window.onkeydown = (event) => {
-    if (event.key === "t" && event.altKey && !event.ctrlKey && !event.shiftKey) {
-      appTheme.toggle()
+  // don't kill me
+  const toggleTheme = () => {
+    const html = document.querySelector('html')
+    const theme = html.getAttribute('theme')
+    if (theme == 'dark') {
+      html.setAttribute('theme', 'light')
+    } else {
+      html.setAttribute('theme', 'dark')
+    }
+  }
+
+  window.onkeydown = event => {
+    if (event.key === 't' && event.altKey && !event.ctrlKey && !event.shiftKey) {
+      // appTheme.toggle() // eita kaj kore na, so amar ta add korte hoiteche. fix it later
+      toggleTheme()
     }
 
-    if (event.key === "/" && !event.altKey && event.ctrlKey && !event.shiftKey) {
+    if (event.key === '/' && !event.altKey && event.ctrlKey && !event.shiftKey) {
       ChatView._chatForm.querySelector(`textarea`).focus()
     }
   }
