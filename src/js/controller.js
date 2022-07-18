69e4716f1586043d1e60094ce84845d14995eaf3
Nazmus Sayad <247sayad@gmail.com>
2022-07-18 22:44:36 +0600

    keyboard shortcut added

--- src/js/controller.js
+++ src/js/controller.js
@@ -108,8 +108,10 @@ const initChat = async () => {
     if (event.key === "t" && event.altKey && !event.ctrlKey && !event.shiftKey) {
       appTheme.toggle()
     }
-    
-    
+
+    if (event.key === "/" && !event.altKey && event.ctrlKey && !event.shiftKey) {
+      ChatView._chatForm.querySelector(`textarea`).focus()
+    }
   }
 })()
 
