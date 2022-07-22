c55a4913f66732d84d573efe3778043453d4a094
Nazmus Sayad <247sayad@gmail.com>
2022-07-23 00:05:49 +0600

    emoji*

--- src/js/views/ChatView.js
+++ src/js/views/ChatView.js
@@ -1,8 +1,6 @@
-import Twemoji from 'twemoji'
-import markup from '../../components/chat.html'
+import markup from "../../components/chat.html"
 
-// push kardu kya? current chole gele pera
-import messageMarkup from '../../components/chatMessage.html'
+import messageMarkup from "../../components/chatMessage.html"
 import {
   getScrollBottom,
   HTML,
@@ -10,8 +8,8 @@ import {
   refactorMessageBeforeSending,
   simplifyDate,
   makeTextReadyForRender,
-} from '../utils.js'
-import { Views } from './Views'
+} from "../utils.js"
+import { Views } from "./Views"
 
 class Chat extends Views {
   constructor() {
@@ -21,13 +19,13 @@ class Chat extends Views {
   _element = (() => {
     const iconSend_SVGColor = `#555`
     const iconSend_SVG = `<svg viewBox="0 0 24 24" class="crt8y2ji"><path d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z" fill="${iconSend_SVGColor}"></path></svg>`
-    const finalMarkup = markup.replace('${Send}', iconSend_SVG)
+    const finalMarkup = markup.replace("${Send}", iconSend_SVG)
 
     return new HTML(finalMarkup)
   })()
 
   _beforeRender() {
-    document.title = 'chat-room #22 | chat'
+    document.title = "chat-room #22 | chat"
     Notification.requestPermission()
   }
 
@@ -54,19 +52,19 @@ class Chat_Form extends Chat {
   _chatForm = this._element.querySelector(`form`)
 
   _textareaResizer(textarea) {
-    textarea.style.height = 'auto'
+    textarea.style.height = "auto"
     const scrollHeight = textarea.scrollHeight
-    textarea.style.height = scrollHeight > 120 ? '120px' : scrollHeight + 'px'
+    textarea.style.height = scrollHeight > 120 ? "120px" : scrollHeight + "px"
   }
 
   addMsgSubmitHandler(callback) {
-    this._chatForm.onsubmit = event => {
+    this._chatForm.onsubmit = (event) => {
       event.preventDefault()
       const { msg } = event.target
       const value = refactorMessageBeforeSending(msg.value)
-      if (value === ' ') return
+      if (value === " ") return
       callback(value)
-      msg.value = ''
+      msg.value = ""
       this._textareaResizer(msg)
     }
   }
@@ -80,19 +78,19 @@ class Chat_Form extends Chat {
       textarea.focus()
     }
 
-    textarea.addEventListener('keydown', event => {
+    textarea.addEventListener("keydown", (event) => {
       if (event.keyCode !== 13 || event.shiftKey || event.ctrlKey) return
       event.preventDefault()
       button.click()
     })
 
-    textarea.addEventListener('input', () => {
+    textarea.addEventListener("input", () => {
       this._textareaResizer(textarea)
 
       if (textarea.value) {
         button.removeAttribute(`disabled`)
       } else {
-        button.setAttribute(`disabled`, '')
+        button.setAttribute(`disabled`, "")
       }
     })
   }
@@ -108,9 +106,7 @@ class Chat_Form_Messages extends Chat_Form {
   _messageContainer = this._element.querySelector(`#chat-container`)
 
   getLastSentMessage() {
-    const firstPendingMessage = this._messageContainer.querySelector(
-      `[data-status="pending"]`
-    )
+    const firstPendingMessage = this._messageContainer.querySelector(`[data-status="pending"]`)
 
     if (firstPendingMessage) {
       return firstPendingMessage.previousElementSibling
@@ -120,7 +116,7 @@ class Chat_Form_Messages extends Chat_Form {
 
   getPendingMessages() {
     const elements = this._messageContainer.querySelectorAll(`[data-status="pending"]`)
-    elements.forEach(element => {
+    elements.forEach((element) => {
       element.msg = element.querySelector(`[text]`).textContent
     })
 
@@ -139,9 +135,7 @@ class Chat_Form_Messages extends Chat_Form {
     text.innerHTML = makeTextReadyForRender(data.msg)
 
     if (data._id) {
-      const isMsgAlreadyRendered = this._messageContainer.querySelector(
-        `[data-id="${data._id}"]`
-      )
+      const isMsgAlreadyRendered = this._messageContainer.querySelector(`[data-id="${data._id}"]`)
       if (isMsgAlreadyRendered) {
         return false
       }
@@ -149,15 +143,15 @@ class Chat_Form_Messages extends Chat_Form {
       element.dataset.id = data._id
       text.title = simplifyDate(data.sent)
     } else {
-      element.dataset.status = 'pending'
-      text.title = 'Sending...'
+      element.dataset.status = "pending"
+      text.title = "Sending..."
     }
 
     if (data.you) {
-      element.dataset.user = 'you'
+      element.dataset.user = "you"
     }
 
-    return Twemoji.parse(element)
+    return element
   }
 
   ifNeedsToScroll() {
@@ -171,7 +165,7 @@ class Chat_Form_Messages extends Chat_Form {
     this._messageContainer.scrollTo({
       top: this._messageContainer.scrollHeight,
       left: 0,
-      behavior: this._loaded ? 'smooth' : 'auto',
+      behavior: this._loaded ? "smooth" : "auto",
     })
   }
 
@@ -188,7 +182,7 @@ class Chat_Form_Messages extends Chat_Form {
 
     if (this.ifNeedsToScroll() || data.you) {
       this.scrollToBottom()
-      if (document.visibilityState === 'hidden' && this._loaded) {
+      if (document.visibilityState === "hidden" && this._loaded) {
         newMessageNotification(data.user, data.msg)
       }
     } else {
@@ -206,20 +200,18 @@ class Chat_Form_Messages extends Chat_Form {
     this._messageContainer.prepend(element)
 
     const scp =
-      this._messageContainer.scrollHeight -
-      scrollBottom -
-      this._messageContainer.clientHeight
+      this._messageContainer.scrollHeight - scrollBottom - this._messageContainer.clientHeight
     this._messageContainer.scrollTo({
       top: scp,
       left: 0,
-      behavior: 'auto',
+      behavior: "auto",
     })
   }
 
   appendMessageSent(element, data) {
     element.dataset.id = data._id
-    element.querySelector('[text]').title = simplifyDate(data.sent)
-    element.dataset.status = 'sent'
+    element.querySelector("[text]").title = simplifyDate(data.sent)
+    element.dataset.status = "sent"
   }
 
   async addLoadMoreHandler(callback) {
@@ -229,7 +221,7 @@ class Chat_Form_Messages extends Chat_Form {
       if (isNoMessageFound) break
     }
 
-    this._messageContainer.onscroll = event => {
+    this._messageContainer.onscroll = (event) => {
       if (event.target.scrollTop === 0) {
         const oldestMessage = this._messageContainer.firstElementChild
         callback(oldestMessage)
